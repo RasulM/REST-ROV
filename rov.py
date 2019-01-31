5b222ff3126d59c6d26ccc2e0aeb9e5c64785dc0
Rasul Mahones <44272827+RasulM@users.noreply.github.com>
2019-01-31 18:02:33 -0500

    Add files via upload

---
+++ rov.py
@@ -0,0 +1,132 @@
+import os   
+import time
+
+os.system ("sudo pigpiod") #Launching GPIO library
+time.sleep(1) #needed delay to setup
+import pigpio #GPIO library
+
+LeftMotor=4 #pin 7
+RightMotor=14 #pin8
+UpMotor=5#pin
+
+
+pi = pigpio.pi();
+pi.set_servo_pulsewidth(LeftMotor, 0)
+pi.set_servo_pulsewidth(RightMotor, 0)
+pi.set_servo_pulsewidth(UpMotor, 0) 
+
+def calibrate(ESCPIN):#This is the auto calibration procedure of a normal ESC
+    max_value = 2000 #change this if your ESC's max value is different or leave it be
+    min_value = 700  #change this if your ESC's min value is different or leave it be
+
+
+
+    pi.set_servo_pulsewidth(ESC, 0)
+    print("Disconnect the battery and press Enter")
+    inp = input()
+    if inp == '':
+        pi.set_servo_pulsewidth(ESC, max_value)
+        print("Connect the battery NOW.. you will here two beeps, then wait for a gradual falling tone then press Enter")
+        inp = input()
+        if inp == '':            
+            pi.set_servo_pulsewidth(ESC, min_value)
+            print ("Wierd eh! Special tone")
+            time.sleep(7)
+            print ("Wait for it ....")
+            time.sleep (5)
+            print ("Im working on it, DONT WORRY JUST WAIT.....")
+            pi.set_servo_pulsewidth(ESC, 0)
+            time.sleep(2)
+            print ("Arming ESC now...")
+            pi.set_servo_pulsewidth(ESC, min_value)
+            time.sleep(1)
+            print ("See.... uhhhhh")
+          # You can change this to any other function you want
+
+def calibrateRovMotor():#This is the auto calibration procedure of a normal ESC
+    max_value = 2000 #change this if your ESC's max value is different or leave it be
+    min_value = 700  #change this if your ESC's min value is different or leave it be
+
+
+
+    pi.set_servo_pulsewidth(LeftMotor, 0)
+    pi.set_servo_pulsewidth(RightMotor, 0)
+    pi.set_servo_pulsewidth(UpMotor, 0)
+    print("Disconnect the battery and press Enter")
+    inp = input()
+    if inp == '':
+        pi.set_servo_pulsewidth(LeftMotor, max_value)
+        pi.set_servo_pulsewidth(RightMotor, max_value)
+        pi.set_servo_pulsewidth(UpMotor, max_value)
+        print("Connect the battery NOW.. you will here two beeps, then wait for a gradual falling tone then press Enter")
+        inp = input()
+        if inp == '':            
+            pi.set_servo_pulsewidth(LeftMotor, min_value)
+            pi.set_servo_pulsewidth(RightMotor, min_value)
+            pi.set_servo_pulsewidth(UpMotor, min_value)
+            print ("Wierd eh! Special tone")
+            time.sleep(7)
+            print ("Wait for it ....")
+            time.sleep (5)
+            print ("Im working on it, DONT WORRY JUST WAIT.....")
+            pi.set_servo_pulsewidth(LeftMotor, 0)
+            pi.set_servo_pulsewidth(RightMotor, 0)
+            pi.set_servo_pulsewidth(UpMotor, 0)
+            time.sleep(2)
+            print ("Arming ESC now...")
+            pi.set_servo_pulsewidth(LeftMotor, min_value)
+            pi.set_servo_pulsewidth(RightMotor, min_value)
+            pi.set_servo_pulsewidth(UpMotor, min_value)
+            time.sleep(1)
+            print("See.... uhhhhh")
+          # You can change this to any other function you want
+            
+def stop(): #This will stop every action your Pi is performing for ESC ofcourse.
+    pi.set_servo_pulsewidth(LeftMotor, 0)
+    pi.set_servo_pulsewidth(RightMotor, 0) 
+    pi.set_servo_pulsewidth(UpMotor, 0)     
+    pi.stop()
+
+def up():
+    print("Up")
+    pi.set_servo_pulsewidth(UpMotor, 1300)
+    
+def down():
+    print("Down")
+    pi.set_servo_pulsewidth(UpMotor, 1700)
+    
+def right():
+    print("Right")
+    pi.set_servo_pulsewidth(RightMotor, 1300)     
+    pi.set_servo_pulsewidth(LeftMotor, 1700)
+    
+def left():
+    print("Left")
+    pi.set_servo_pulsewidth(RightMotor, 1700)     
+    pi.set_servo_pulsewidth(LeftMotor, 1300)
+
+def backward():
+    print("Backward")
+    pi.set_servo_pulsewidth(RightMotor, 1700)     
+    pi.set_servo_pulsewidth(LeftMotor, 1700)     
+
+def forward():
+    print("Forward")
+    pi.set_servo_pulsewidth(RightMotor, 1300)     
+    pi.set_servo_pulsewidth(LeftMotor, 1300)
+    
+def still():
+    print("Still...")
+    pi.set_servo_pulsewidth(RightMotor, 1500)     
+    pi.set_servo_pulsewidth(LeftMotor, 1500)
+    pi.set_servo_pulsewidth(UpMotor, 1500)
+
+def openclaw():
+    print("Open")
+    
+def closeclaw():
+    print("Close")
+    
+    
+#leftstickFBLR BClawOC RTUp LTDown
+forward()
---
+++ test4.py
@@ -0,0 +1,77 @@
+#import evdev
+from evdev import InputDevice, categorize, ecodes
+import rov
+
+#creates object 'gamepad' to store the data
+#you can call it whatever you like
+gamepad = InputDevice('/dev/input/event0')
+
+#button code variables (change to suit your device)
+aBtn = 34
+bBtn = 305
+xBtn = 35
+yBtn = 23
+
+up = 46
+down = 32
+left = 18
+right = 33
+
+start = 24
+select = 49
+
+lTrig = 310
+rTrig = 311
+
+LX = 0
+LY = 1
+#prints out device info at start
+print(gamepad)
+
+#loop and filter by event code and print the mapped label
+for event in gamepad.read_loop():
+    if event.type == ecodes.EV_ABS:
+        if event.value == 0:
+            if event.code == LX:
+                rov.left()
+            elif event.code == LY:
+                rov.up()
+        elif event.value == 255:
+            if event.code == LX:
+                rov.right()
+            elif event.code == LY:
+                rov.down()
+            
+    if event.type == ecodes.EV_KEY:
+        if event.value == 1:
+          #  if event.code == yBtn:
+            #    print("Y")
+                
+            if event.code == bBtn:
+                print("B lit fm")
+                rov.closeclaw()
+                
+            elif event.code == aBtn:
+               print("A")
+               rov.openclaw()
+          #  elif event.code == xBtn:
+           #     print("X")
+
+           # elif event.code == up:
+           #     print("up")
+           # elif event.code == down:
+             #   print("down")
+           # elif event.code == left:
+           #     print("left")
+           # elif event.code == right:
+             #   print("right")
+
+          #  elif event.code == start:
+          #      print("start")
+          #  elif event.code == select:
+            #    print("select")
+
+            elif event.code == lTrig:
+                print("left trigger gam")
+            elif event.code == rTrig:
+                print("right trigger tduyis")
