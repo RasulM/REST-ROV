fe3b847a923d4d59188dd33583195f74f0160faf
Rasul Mahones <44272827+RasulM@users.noreply.github.com>
2019-04-12 18:49:47 -0400

    Update rov.py

--- rov.py
+++ rov.py
@@ -4,124 +4,84 @@ import time
 os.system ("sudo pigpiod") #Launching GPIO library
 time.sleep(1) #needed delay to setup
 import pigpio #GPIO library
-
-LeftMotor=4 #pin 7
-RightMotor=14 #pin8
-UpMotor=24#pin18
-Claw= 23#pin16 -> m2
+#White motor controller
+LIn1=4#pin 7
+LIn2=3#pin 5
+LEnA=2#pin 3
+RIn3=14#pin8
+RIn4=15#pin 10
+REnB=18#pin 12
+#black motor controller
+UpM=24#pin18 m1
+UpEn=10#pin19 e1
+Claw=23#pin16 -> m2
 En= 22#pin15 -> e2
 
 
 pi = pigpio.pi();
-pi.set_servo_pulsewidth(LeftMotor, 0)
-pi.set_servo_pulsewidth(RightMotor, 0)
-pi.set_servo_pulsewidth(UpMotor, 0) 
 
-def calibrate(ESCPIN):#This is the auto calibration procedure of a normal ESC
-    max_value = 2000 #change this if your ESC's max value is different or leave it be
-    min_value = 700  #change this if your ESC's min value is different or leave it be
-
-
-
-    pi.set_servo_pulsewidth(ESC, 0)
-    print("Disconnect the battery and press Enter")
-    inp = input()
-    if inp == '':
-        pi.set_servo_pulsewidth(ESC, max_value)
-        print("Connect the battery NOW.. you will here two beeps, then wait for a gradual falling tone then press Enter")
-        inp = input()
-        if inp == '':            
-            pi.set_servo_pulsewidth(ESC, min_value)
-            print ("Wierd eh! Special tone")
-            time.sleep(7)
-            print ("Wait for it ....")
-            time.sleep (5)
-            print ("Im working on it, DONT WORRY JUST WAIT.....")
-            pi.set_servo_pulsewidth(ESC, 0)
-            time.sleep(2)
-            print ("Arming ESC now...")
-            pi.set_servo_pulsewidth(ESC, min_value)
-            time.sleep(1)
-            print ("See.... uhhhhh")
-          # You can change this to any other function you want
-
-def calibrateRovMotor():#This is the auto calibration procedure of a normal ESC
-    max_value = 2000 #change this if your ESC's max value is different or leave it be
-    min_value = 700  #change this if your ESC's min value is different or leave it be
-
-
-
-    pi.set_servo_pulsewidth(LeftMotor, 0)
-    pi.set_servo_pulsewidth(RightMotor, 0)
-    pi.set_servo_pulsewidth(UpMotor, 0)
-    print("Disconnect the battery and press Enter")
-    inp = input()
-    if inp == '':
-        pi.set_servo_pulsewidth(LeftMotor, max_value)
-        pi.set_servo_pulsewidth(RightMotor, max_value)
-        pi.set_servo_pulsewidth(UpMotor, max_value)
-        print("Connect the battery NOW.. you will here two beeps, then wait for a gradual falling tone then press Enter")
-        inp = input()
-        if inp == '':            
-            pi.set_servo_pulsewidth(LeftMotor, min_value)
-            pi.set_servo_pulsewidth(RightMotor, min_value)
-            pi.set_servo_pulsewidth(UpMotor, min_value)
-            print ("Wierd eh! Special tone")
-            time.sleep(7)
-            print ("Wait for it ....")
-            time.sleep (5)
-            print ("Im working on it, DONT WORRY JUST WAIT.....")
-            pi.set_servo_pulsewidth(LeftMotor, 0)
-            pi.set_servo_pulsewidth(RightMotor, 0)
-            pi.set_servo_pulsewidth(UpMotor, 0)
-            time.sleep(2)
-            print ("Arming ESC now...")
-            pi.set_servo_pulsewidth(LeftMotor, min_value)
-            pi.set_servo_pulsewidth(RightMotor, min_value)
-            pi.set_servo_pulsewidth(UpMotor, min_value)
-            time.sleep(1)
-            print("See.... uhhhhh")
-          # You can change this to any other function you want
             
 def stop(): #This will stop every action your Pi is performing for ESC ofcourse.
-    pi.set_servo_pulsewidth(LeftMotor, 0)
-    pi.set_servo_pulsewidth(RightMotor, 0) 
-    pi.set_servo_pulsewidth(UpMotor, 0)     
     pi.stop()
 
 def up():
     print("Up")
-    pi.set_servo_pulsewidth(UpMotor, 1300)
+    pi.write(UpMotor, 1)
+    pi.write(UpEn,1)
+    time.sleep(2)
+    pi.write(UpEn,0)
     
 def down():
     print("Down")
-    pi.set_servo_pulsewidth(UpMotor, 1700)
+    pi.write(UpM, 0)
+    pi.write(UpEn,1)
+    time.sleep(2)
+    pi.write(UpEn,0)
     
 def right():
     print("Right")
-    pi.set_servo_pulsewidth(RightMotor, 1300)     
-    pi.set_servo_pulsewidth(LeftMotor, 1700)
-    
+    pi.write(RIn1,0)
+    pi.write(RIn2,1)
+    pi.write(REnB,1)
+    time.sleep(2)
+    pi.write(REnB,0)
+
 def left():
     print("Left")
-    pi.set_servo_pulsewidth(RightMotor, 1700)     
-    pi.set_servo_pulsewidth(LeftMotor, 1300)
+    pi.write(LIn1,0)
+    pi.write(LIn2,1)
+    pi.write(LEnB,1)
+    time.sleep(2)
+    pi.write(LEnB,0)
 
 def backward():
     print("Backward")
-    pi.set_servo_pulsewidth(RightMotor, 1700)     
-    pi.set_servo_pulsewidth(LeftMotor, 1700)     
+    pi.write(RIn1,0)
+    pi.write(RIn2,1)
+    pi.write(LIn1,0)
+    pi.write(LIn2,1)
+    pi.write(LEnB,1)
+    pi.write(REnB,1)
+    time.sleep(2)
+    pi.write(REnB,0)   
+    pi.write(LEnB,0)
 
 def forward():
     print("Forward")
-    pi.set_servo_pulsewidth(RightMotor, 1300)     
-    pi.set_servo_pulsewidth(LeftMotor, 1300)
+    pi.write(RIn1,1)
+    pi.write(RIn2,0)
+    pi.write(LIn1,1)
+    pi.write(LIn2,0)
+    pi.write(LEnB,1)
+    pi.write(REnB,1)
+    time.sleep(2)
+    pi.write(REnB,0)   
+    pi.write(LEnB,0)
     
 def still():
     print("Still...")
-    pi.set_servo_pulsewidth(RightMotor, 1500)     
-    pi.set_servo_pulsewidth(LeftMotor, 1500)
-    pi.set_servo_pulsewidth(UpMotor, 1500)
+    pi.write(REnB,0)   
+    pi.write(LEnB,0)
 
 def openclaw():
     print("Open")
@@ -135,7 +95,3 @@ def closeclaw():
     pi.write(En,1)
     time.sleep(2)
     pi.write(En,0)
-    
-    
-#leftstickFBLR BClawOC RTUp LTDown
-
